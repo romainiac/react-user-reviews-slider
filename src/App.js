8eeba4aa97d1385719fe3d3c226c70decb47dfb9
unknown <romanyefimets@gmail.com>
2022-02-22 11:15:36 -0800

    added button functionality to slide to next person and 3 second auto slide

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import data from './data';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaQuoteRight } from 'react-icons/fa';
 import { FiChevronLeft } from 'react-icons/fi';
 import { FiChevronRight } from 'react-icons/fi';
 const App = () => {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) setIndex(lastIndex);
+    if (index > lastIndex) setIndex(0);
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
 
   return (
     <section className="section">
@@ -31,10 +44,10 @@ const App = () => {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
